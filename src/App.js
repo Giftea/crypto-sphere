47e945c14b83b8030c231fc4dd3c8389addf5f35
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-09-19 15:14:11 +0100

    initialized components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar, Homepage,Exchanges,Cryptocurrencies, CryptoDetails,News} from "./components";
+
 function App() {
   return (
-    <div>
-
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinid'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 }
